9f2fca47fe6cc524e76c8b864a72929cbc2ba410
mohamed <mohamed@Ubuntu.myguest.virtualbox.org>
2024-02-01 23:18:12 +0200

    .

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      resolve("random ");
-    });
-  }
-  
+  return new Promise((resolve) => {
+    resolve('random');
+  });
+}
