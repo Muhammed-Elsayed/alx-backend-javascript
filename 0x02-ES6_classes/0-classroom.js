518de2ae456167f7f85d5f8eb8f50eea6203e261
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-02-03 23:41:28 +0200

    0, 1

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+export default class ClassRoom {
+  constructor(value){
+    this._maxStudentsSize = value
+  }
+}
---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
