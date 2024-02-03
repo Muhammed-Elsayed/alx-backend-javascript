90966a82d4ed308bea8e45a6eca2e7166cbf3c10
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-02-04 01:15:38 +0200

    6

--- 0x02-ES6_classes/3-main.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-import Currency from "./3-currency.js";
-
-const dollar = new Currency('$', 'Dollars');
-console.log(dollar.displayFullCurrency());
---
+++ 0x02-ES6_classes/7-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import Airport from "./7-airport.js";
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
