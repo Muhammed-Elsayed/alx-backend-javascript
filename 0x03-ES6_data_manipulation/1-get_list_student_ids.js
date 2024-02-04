f77cba59255949d0932df02cb889758fe8da87ec
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-02-04 23:49:06 +0200

    1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,9 @@
 /* eslint-disable */
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map(function(obj){
+      return obj.id
+    });
+  }
+  return [];
+}
